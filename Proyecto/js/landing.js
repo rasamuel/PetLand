45d5190ac7293cfa8c282c97da7822fbfedb0929
rasamuel <142336513+rasamuel@users.noreply.github.com>
2024-08-06 09:36:48 -0500

    Actualización

--- Proyecto/js/landing.js
+++ Proyecto/js/landing.js
@@ -8,3 +8,28 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
   
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío del formulario
+
+    // Obtiene los valores del formulario
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    // Valida los campos
+    if (!name || !email || !message) {
+        displayResponseMessage('Por favor, complete todos los campos.');
+        return;
+    }
+
+    // Muestra un mensaje de éxito
+    displayResponseMessage('¡Mensaje enviado con éxito!');
+
+    // Limpia el formulario
+    document.getElementById('contactForm').reset();
+});
+
+function displayResponseMessage(message) {
+    const responseMessageElement = document.getElementById('responseMessage');
+    responseMessageElement.textContent = message;
+}
